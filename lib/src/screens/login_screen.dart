12551340e0f442d0a01bea83b5efd2dccbbb2541
sylvain <sylvain@stilde.fr>
2019-01-31 10:59:39 +0100

    first commit

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  createState() {    
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  
+  Widget build(context){
+
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('login screen'),
+    );
+  }
+}
